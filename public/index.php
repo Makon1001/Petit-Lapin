f543f0b885f6d4211839c45924ceeda229671b2f
Sebastien Guerin <sebast.guerin@gmail.com>
2019-04-17 19:42:29 +0200

    création classe Personnage

--- public/index.php
+++ public/index.php
@@ -4,6 +4,13 @@ include '../src/views/inc_head.php';
 include '../src/views/connexionApi.php';
 
 // content
-var_dump($test = selectAllPerso());
+
+$perso1 = new \perso\Personnage('John', 5, 5, 0,0);
+$perso2 = new \perso\Personnage('Jane',3,9,1,2);
+
+
+echo $perso2->status();
+$perso1->attack($perso2);
+echo $perso2->status();
 
 include '../src/views/inc_script.php';
