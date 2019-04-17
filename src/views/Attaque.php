7e4ee6416b108a3ae734d805975b2eff8d1d9149
Tribu <Quentin_Tribu_student2019@wilder.school>
2019-04-17 23:27:04 +0200

    Page attaque code epuré

--- src/views/Attaque.php
+++ src/views/Attaque.php
@@ -9,16 +9,16 @@ include "./inc_head.php";
             <div class="row d-flex justify-content-center">  
                 <div class="card text-center col-md-6">
                     <div>
-                        <h1>Joueur 1</h1>
+                        <h1 class="py-2 pb-2">Joueur 1</h1>
                     </div>
                     <div>
-                        <img src="1.jpeg" alt="1">
+                        <img class="py-2 mb-2" src="1.jpeg" alt="1">
                     </div>
                     <div>
-                        <a href="#" class="btn btn-info" role="button">Attaque</a>
+                        <a href="#" class="btn btn-info mb-2" role="button">Attaque</a>
                     </div>
                     <div>
-                        <a href="#" class="btn btn-info" role="button">Se soigner</a>
+                        <a href="#" class="btn btn-info py-2 pb-2 mb-2" role="button">Se soigner</a>
                     </div>                    
                     <div>
                         <a href="#" class="btn btn-info" role="button">Defense</a>
@@ -26,16 +26,16 @@ include "./inc_head.php";
                 </div>
                 <div class="card text-center col-md-6">
                     <div>
-                        <h1>Joueur 2</h1>
+                        <h1 class="py-2 pb-2">Joueur 2</h1>
                     </div>
                     <div>
-                        <img src="1.jpeg" alt="1">
+                        <img class="py-2 mb-2" src="27.jpeg" alt="27">
                     </div>
                     <div>
-                        <a href="#" class="btn btn-info" role="button">Attaque</a>
+                        <a href="#" class="btn btn-info mb-2" role="button">Attaque</a>
                     </div>
                     <div>
-                        <a href="#" class="btn btn-info" role="button">Se soigner</a>
+                        <a href="#" class="btn btn-info py-2 pb-2 mb-2" role="button">Se soigner</a>
                     </div>                    
                     <div>
                         <a href="#" class="btn btn-info" role="button">Defense</a>
