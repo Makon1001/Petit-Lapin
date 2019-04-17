420486374d6137c8bb7b3f5d0a5c7e56a6d77a44
Manuel Konrath <manuel.konrath@gmail.com>
2019-04-17 21:01:46 +0200

    ajout de la classe eggs

--- src/controller/ApiController.php
+++ src/controller/ApiController.php
@@ -8,7 +8,7 @@ use GuzzleHttp\Client;
 
 class ApiController
 {
-    function selectAllPerso() {
+    public function selectAllPerso() {
         $client = new GuzzleHttp\Client([
             'base_uri' => 'http://easteregg.wildcodeschool.fr/api/'
         ]);
@@ -18,5 +18,23 @@ class ApiController
         $body = $response->getBody();
 
         return json_decode($body->getContents());
+    }
+
+    public function selectFourRandomEggs (){
+        $arrEggs = array();
+
+        for($i=0;$i<4;$i++) {
+            $client = new GuzzleHttp\Client([
+                'base_uri' => 'http://easteregg.wildcodeschool.fr/api/'
+            ]);
+            $response = $client->request('GET', 'eggs/random');
+            // or
+
+            $body = $response->getBody();
+
+            $arrEggs[$i] = json_decode($body->getContents());
+        }
+        return $arrEggs;
+
     }
 }
