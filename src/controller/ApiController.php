bcdeb61650c8d5c12c8e800482122651de010508
Sebastien Guerin <sebast.guerin@gmail.com>
2019-04-17 19:51:27 +0200

    init ApiController. supression PersoController

---
+++ src/controller/ApiController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace perso;
+
+
+use GuzzleHttp\Client;
+
+class ApiController
+{
+    function selectAllPerso() {
+        $client = new GuzzleHttp\Client([
+            'base_uri' => 'http://easteregg.wildcodeschool.fr/api/'
+        ]);
+        $response = $client->request('GET', 'characters');
+        // or
+
+        $body = $response->getBody();
+
+        return json_decode($body->getContents());
+    }
+}
