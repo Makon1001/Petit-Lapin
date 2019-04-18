ebad909f09f1d980fea6ad82c6cb79999012f932
ChocW <julienduartep@gmail.com>
2019-04-18 12:30:27 +0200

    chocW

--- public/defaite.php
+++ public/defaite.php
@@ -1,7 +1,6 @@
 <?php
 include './inc_head.php';
 include './connexionApi.php';
-include './inc_script.php';
 ?>
 <header class="jumbotron text-center text-white bg-dark">
     <h2>Petits Lapins Prod vous à présenté</h2>
@@ -10,8 +9,9 @@ include './inc_script.php';
     <div class="container py-5">
         <div class="row py-5 text-center">
             <div class="col-sm-12">
-                <h1>EGGS FIGHTERS</h1>
-                </br><p>vous avaiz perdu!</p>
+                <h1>EGGS FIGHTERS</h1></br></br>
+                <h3>Yavuz vous a infligé 6000 de dégats!</h3>
+                </br><p>vous avez perdu!</p>
                 <p>il ne vous reste plus que vos yeux pour pleurer...</p>
                 <img src ="https://cdn.discordapp.com/attachments/568117769548333067/568348557447528451/Photo_-_02019-13-18-10-13-46.jpg">
                 <p>vous ferait mieux la prochaine fois! OU PAS!!!</p></br>
@@ -31,3 +31,7 @@ include './inc_script.php';
     </div>
 
 </footer>
+
+<?php
+include './inc_script.php';
+?>
