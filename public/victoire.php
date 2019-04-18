cdad1f54b79fc10b439e14fbf795b024e99913cc
mathieu <drabland@gmail.com>
2019-04-18 14:06:24 +0200

    x

--- public/victoire.php
+++ public/victoire.php
@@ -13,13 +13,13 @@ session_destroy ();
             <div class="row py-5 text-center">
                 <div class="col-sm-12">
                     <h1>EGGS FIGHTERS</h1>
-                    </br><p>vous avaiz gagne face a Yavuz grace a l'@!</p>
+                    </br><p>vous avez gagne face a Yavuz grace a l'@!</p>
                     <p>le jeu est terminé!</p>
                     <img src ="https://www.assoedc.com/wp-content/uploads/2015/09/EDC-Victoire-EnR.jpg">
                     <p>merci d'avoir joué au jeu!</p></br>
                 </div>
                 <div class="col-md-12">
-                    <a href="index.php" class="btn btn-info mb-2" role="button">Fight!!!</a>
+                    <a href="index.php" class="btn btn-info mb-2" role="button">Réessayer</a>
                 </div>
             </div>
         </div>
