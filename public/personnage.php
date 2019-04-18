ea2891ea7e906794766bada3c8be081d7ebe0ca7
mathieu <drabland@gmail.com>
2019-04-18 03:00:26 +0200

    xx

--- public/personnage.php
+++ public/personnage.php
@@ -12,7 +12,34 @@ function selectRandomPerso() {
 
     return json_decode($body->getContents());
 }
+function recupChampion1(){
+    if(isset( $_SESSION['player1']['champion'])){
+        $client = new GuzzleHttp\Client([
+            'base_uri' => 'http://easteregg.wildcodeschool.fr/api/'
+        ]);
+        $concat= "characters/".$_SESSION['player1']['champion'];
+        $response = $client->request('GET',$concat );
 
+        $body = $response->getBody();
+        $champion=get_object_vars(json_decode($body->getContents()));
+        $_SESSION['player1']['imgSrc']=$champion["image"];
+        return '<img src='.$_SESSION['player1']['imgSrc'].' alt="..." class="persoImg">';
+    }
+}
+function recupChampion2(){
+    if(isset( $_SESSION['player2']['champion'])){
+        $client = new GuzzleHttp\Client([
+            'base_uri' => 'http://easteregg.wildcodeschool.fr/api/'
+        ]);
+        $concat= "characters/".$_SESSION['player2']['champion'];
+        $response = $client->request('GET',$concat );
+
+        $body = $response->getBody();
+        $champion=get_object_vars(json_decode($body->getContents()));
+        $_SESSION['player2']['imgSrc']=$champion["image"];
+        return '<img src='.$_SESSION['player2']['imgSrc'].' alt="..." class="persoImg">';
+    }
+}
 
 require '../vendor/autoload.php';
 include 'inc_head.php';
@@ -31,21 +58,27 @@ function bouton(){
         <div class="container_fluid">
             <div class="row">
                 <div class="col-md-3 text-center">
-                    <h2>Player One</h2>
+                    <h2><?php echo $_SESSION['player1']['name'];?></h2>
+                    <?php if(!empty($_SESSION["player1"]["champion"])){
+                        echo recupChampion1();
+                    } ?>
                 </div>
                 <div class="col-md-6">
                     <div class="row">
                         <?php
-                        for ($i=1;$i<=12;$i++){
-                            $value[$i]=selectRandomPerso();
+                        for ($i = 1; $i <= 12; $i++) {
+                            $value[$i] = selectRandomPerso();
                             $value[$i] = get_object_vars($value[$i]);
-                            echo '<div class="col-2"><a href="?id='.$value[$i]["id"].'" ><img src="'.$value[$i]["image"].'" alt="..." class="img-thumbnail"><p>'.$value[$i]["name"].'</p></a></div>';
+                            echo '<div class="col-2"><a href="?id=' . $value[$i]["id"] . '" ><img src="' . $value[$i]["image"] . '" alt="..." class="img-thumbnail"><p>' . $value[$i]["name"] . '</p></a></div>';
                         }
                         ?>
                     </div>
                 </div>
                 <div class="col-md-3 text-center">
-                    <h2>Player Two</h2>
+                    <h2><?php echo $_SESSION['player2']['name']; ?></h2>
+                    <?php if(!empty($_SESSION["player2"]["champion"])){
+                        echo recupChampion2();
+                    } ?>
                 </div>
             </div>
             <div class="row justify-content-center pt-5">
@@ -56,28 +89,31 @@ function bouton(){
 <?php
 function checkplayer()
 {
-    if(empty($_SESSION["player1"]["champion"])) {
+    if (empty($_SESSION["player1"]["champion"])) {
         echo 'Choix player 1';
-    }else{
+    } else {
         echo 'Choix player 2';
     }
 }
-function recupIdChampion(){
-    if(isset($_GET['id'])){
-        $id=$_GET['id'];
+
+function recupIdChampion()
+{
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
         return $id;
     }
 }
-function choixPlayer(){
-    if(isset($_GET['submit'])){
-        if(empty($_SESSION["player1"]["champion"])){
-            $_SESSION["player1"]["champion"]=$_GET['submit'];
+
+function choixPlayer()
+{
+    if (isset($_GET['submit'])) {
+        if (empty($_SESSION["player1"]["champion"])) {
+            $_SESSION["player1"]["champion"] = $_GET['submit'];
             return $_SESSION["player1"]["champion"];
-        }else{
-            $_SESSION["player2"]["champion"]=$_GET['submit'];
+        } else {
+            $_SESSION["player2"]["champion"] = $_GET['submit'];
             return $_SESSION["player2"]["champion"];
         }
     }
 }
 
-include 'inc_script.php';
