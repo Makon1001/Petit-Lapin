132b97fb5f3c959658428bc45b1fb89a4e0b637b
ChocW <julienduartep@gmail.com>
2019-04-18 11:57:12 +0200

    chocV

---
+++ public/victoire.php
@@ -0,0 +1,38 @@
+<?php
+include './inc_head.php';
+include './connexionApi.php';
+include './inc_script.php';
+?>
+<header class="jumbotron text-center text-white bg-dark">
+    <h2>Petits Lapins Prod vous à présenté</h2>
+</header>
+<body class =fun-color>
+    <section>
+        <div class="container py-5">
+            <div class="row py-5 text-center">
+                <div class="col-sm-12">
+                    <h1>EGGS FIGHTERS</h1>
+                    </br><p>vous avaiz gagne!</p>
+                    <p>le jeu est terminé!</p>
+                    <img src ="https://www.assoedc.com/wp-content/uploads/2015/09/EDC-Victoire-EnR.jpg">
+                    <p>merci d'avoir joué au jeu!</p></br>
+                    <div>
+                    <button class="btn btn-primary" value="submit" type="submit" id="index-btn">rejouer</button>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+</body>
+    <hr class="m-0">
+<footer class="text-center py-5">
+    <div class="container ">
+        <p>Hackathon 1</p>
+    </div>
+
+</footer>
+<?php 
+include './inc_script.php'; 
+?>
